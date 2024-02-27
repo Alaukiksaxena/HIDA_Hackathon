c7a250ccae39811cd7aa8d3d8265fb8cc7637100
Alaukiksaxena <saxenaalaukik93@gmail.com>
2024-02-27 22:20:56 +0100

    changes by examiners finalised

--- HIDA-Hackathon-main/predict.sh
+++ HIDA-Hackathon-main/predict.sh
@@ -8,18 +8,15 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=76
 #SBATCH --time=01:00:00
-#SBATCH --reservation=hida
 
-echo AAAA
 export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=76
 
-echo BBB
 data_workspace=/hkfs/work/workspace_haic/scratch/qx6387-hida-hackathon-data
-group_workspace=/hkfs/work/workspace_haic/scratch/qx6387-WetKoalas
+group_workspace=/hkfs/work/workspace_haic/scratch/qx6387-<groupd_id>
 
 module purge
 module load toolkit/nvidia-hpc-sdk/23.9
 
-source ${group_workspace}/HIDA-Hackathon/hida_venv/bin/activate
-python ${group_workspace}/HIDA-Hackathon/predict.py ${data_workspace}/train
+source ${group_workspace}/hida_venv/bin/activate
+srun python ${group_workspace}/HIDA-Hackathon/predict.py ${data_workspace}/train
--- HIDA-Hackathon-main/train.sh
+++ HIDA-Hackathon-main/train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --job-name=training
 #SBATCH --partition=normal
 #SBATCH --nodes=1
@@ -9,16 +10,14 @@
 #SBATCH --time=03:00:00
 #SBATCH --reservation=hida
 
-echo AAAA
 export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=76
 
-echo BBB
 data_workspace=/hkfs/work/workspace_haic/scratch/qx6387-hida-hackathon-data
-group_workspace=/hkfs/work/workspace_haic/scratch/qx6387-WetKoalas
+group_workspace=/hkfs/work/workspace_haic/scratch/qx6387-<group_id>
 
 module purge
 module load toolkit/nvidia-hpc-sdk/23.9
 
-source ${group_workspace}/HIDA-Hackathon/hida_venv/bin/activate
-python ${group_workspace}/HIDA-Hackathon/train.py
+source ${group_workspace}/hida_venv/bin/activate
+srun python ${group_workspace}/HIDA-Hackathon/train.py
