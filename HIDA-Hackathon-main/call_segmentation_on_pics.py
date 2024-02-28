2034e7dfd8bd0795c3abf73b6911cbbfd522f803
Jonas Schaible <118978888+jnitzz@users.noreply.github.com>
2024-02-28 13:54:23 +0100

    update call segmentation, a bit of documentation

--- HIDA-Hackathon-main/call_segmentation_on_pics.py
+++ HIDA-Hackathon-main/call_segmentation_on_pics.py
@@ -5,8 +5,16 @@ Created on Wed Feb 28 13:13:20 2024
 @author: a3536
 """
 
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# https://github.com/facebookresearch/segment-anything visit site
+# download model checkpoint: https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+# pip install git+https://github.com/facebookresearch/segment-anything.git, get segment-anything
+
+# set path in segmentation_on_pics:
+# checkpoint=r"d:\Profile\a3536\Eigene Dateien\GitHub\HIDA_Hackathon\data\segment-anything\sam_vit_h_4b8939.pth"
+
+
+# import os
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
 from segmentation_on_pics import segmentation_on_pics
 
 path_to_pics_folder = r'd:\Profile\a3536\Eigene Dateien\GitHub\HIDA_Hackathon\data\pics_train'
